51f62f5b31a2a311ed4446d6ecb8d033b4845232
Charles Candelaria <charles-can@outlook.com>
2023-05-20 12:37:18 -0600

    moves chart gen to function

--- lib/finance/stock_price.py
+++ lib/finance/stock_price.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
+
 class StockPrice:
-    def __init__(self, symbol='', date='', open='-', high='-', low='-', close='-', volume=0 ):
+    def __init__(self, symbol='', date='', open='-', high='-', low='-', close='-', volume=0):
         self.id = None
         self.symbol = symbol
         self.date = date
@@ -16,5 +17,6 @@ class StockPrice:
         try:
             return datetime.strptime(self.date, '%d-%b-%y')
         except Exception as e:
-            print(f'StockPrice:: failed to parse date for price: [{self.id}]\n{str(e)}')
+            print(
+                f'StockPrice:: failed to parse date for price: [{self.id}]\n{str(e)}')
             return datetime.today()
--- main.py
+++ main.py
@@ -3,11 +3,9 @@
     Date: 05/07/2023
     Functionality: Imports csv stocks and bonds files and outputs a financial report.
 """
-from datetime import datetime
-
 import sqlite3
-import matplotlib.pyplot as plt
-import pandas as pd
+from datetime import datetime
+from matplotlib import pyplot as plt, figure as fig, axes
 
 from lib.finance import Investor
 from lib.utils import import_bonds, import_stocks, import_stock_price_history
@@ -33,11 +31,13 @@ def insert_stocks_from_file(file_path: str, investor_id: int):
         stock.investor_id = investor_id
         stock_store.insert(stock)
 
+
 def insert_bonds_from_file(file_path: str, investor_id: int):
     bonds = import_bonds(file_path)
     for bond in bonds:
         bond.investor_id = investor_id
         bond_store.insert(bond)
+
 
 def insert_prices_from_file(file_path: str):
     prices = import_stock_price_history(file_path)
@@ -46,6 +46,21 @@ def insert_prices_from_file(file_path: str):
         prices_store.insert(price)
 
 
+def build_stock_plot_chart_by_investor(investor: Investor) -> tuple[fig.Figure, axes.Axes]:
+    fig, ax = plt.subplots()
+
+    stocks = stock_store.select_by_investor_id(investor.id)
+
+    for stock in stocks:
+        prices = prices_store.select_by_symbol(stock.symbol)
+        dates = [pr.recorded_date for pr in prices]
+        stock_values = [float(pr.close) * stock.shares for pr in prices]
+
+        plt.plot(dates, stock_values, label=stock.symbol)
+
+    ax.xaxis.set_major_locator(plt.MaxNLocator(8))
+    return (fig, ax)
+
 
 def main():
 
@@ -53,23 +68,13 @@ def main():
     investor = Investor(name='Bob Smith', address='123 fake St.',
                         phone_number='123-123-1234')
     investor_store.insert(investor)
-    
+
     insert_stocks_from_file(STOCKS_FILE_PATH, investor.id)
     insert_bonds_from_file(BONDS_FILE_PATH, investor.id)
     insert_prices_from_file(STOCK_PRICES_FILE_PATH)
 
-    fig, ax = plt.subplots()
+    build_stock_plot_chart_by_investor(investor)
 
-    my_stocks = stock_store.select_by_investor_id(investor.id)
-
-    for st in my_stocks:
-        st_prices = prices_store.select_by_symbol(st.symbol)
-        dates = [pr.recorded_date for pr in st_prices]
-        values = [float(pr.close) * st.shares for pr in st_prices]
-
-        plt.plot(dates, values, label=st.symbol)
-
-    ax.xaxis.set_major_locator(plt.MaxNLocator(8))
     plt.legend(loc='upper left')
     plt.show()
 
