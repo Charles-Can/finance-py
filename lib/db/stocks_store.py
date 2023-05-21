9987d7d37a36340b61e4db81165727de98005b38
Charles Candelaria <charles-can@outlook.com>
2023-05-20 20:50:36 -0600

    StocksStore error handling and doco

--- lib/db/stocks_store.py
+++ lib/db/stocks_store.py
@@ -1,9 +1,15 @@
+"""
+    Author: Charles Candelaria
+    Date: 05/20/2023
+    Functionality: Handles ORM operations for Stock objects with SQLlite
+"""
 from .table_object import TableObject
 from ..utils.mapper import PropertyMapper
 from ..finance import Stock
 
 
 class StocksStore(TableObject):
+    """SQLlite storage api for Stocks"""
 
     def __init__(self, connection):
         super().__init__(connection)
@@ -15,12 +21,13 @@ class StocksStore(TableObject):
             .add_mapping(3, 'shares') \
             .add_mapping(4, 'purchase_price') \
             .add_mapping(5, 'current_value') \
-            .add_mapping(6, 'purchase_date') \
-
+            .add_mapping(6, 'purchase_date')
+        """Holds stocks table tuple mapping to Stock"""
 
     def __create_table__(self):
+        """Creates stocks table"""
         self.q.execute("""
-            CREATE TABLE stocks (
+            CREATE TABLE IF NOT EXISTS stocks (
                 investor_id INT NOT NULL,
                 symbol TEXT NOT NULL,
                 shares INT,
@@ -32,22 +39,42 @@ class StocksStore(TableObject):
         self.connection.commit()
 
     def insert(self, stock: Stock):
-        q = self.q
-        q.execute("INSERT INTO stocks VALUES (?, ?, ?, ?, ?, ?)",
-                  [stock.investor_id, stock.symbol, stock.shares, stock.purchase_price, stock.current_value, stock.purchase_date])
-        stock.id = q.lastrowid
-        self.connection.commit()
+        """Insert a stock into store, updates id on success"""
+        try:
+            q = self.q
+            q.execute("INSERT INTO stocks VALUES (?, ?, ?, ?, ?, ?)",
+                      [stock.investor_id,
+                       stock.symbol,
+                       stock.shares,
+                       stock.purchase_price,
+                       stock.current_value,
+                       stock.purchase_date])
+            stock.id = q.lastrowid
+            self.connection.commit()
+        except Exception as e:
+            print(f'StockStore:: failed insert Stock\n{str(e)}')
 
     def select_by_investor_id(self, id) -> list[Stock]:
-        q = self.q
-        data = q.execute("""SELECT rowid AS id, investor_id, symbol, shares, purchase_price, purchase_price, purchase_date 
-                            FROM stocks 
-                            WHERE investor_id = ?""",
-                         [id]).fetchall()
+        """Retrieves Stocks from store by investor id"""
         stocks = []
-        for record in data:
-            stock = Stock()
-            self.__mapper.map_properties(record, stock)
-            stocks.append(stock)
+        try:
 
+            q = self.q
+            data = q.execute("""SELECT  rowid AS id, 
+                                        investor_id, 
+                                        symbol, 
+                                        shares, 
+                                        purchase_price, 
+                                        purchase_price, 
+                                        purchase_date 
+                                FROM    stocks 
+                                WHERE   investor_id = ?""",
+                             [id]).fetchall()
+            for record in data:
+                stock = Stock()
+                self.__mapper.map_properties(record, stock)
+                stocks.append(stock)
+        except Exception as e:
+            print(
+                f'StocksStore:: Failed to retrieve stocks for investor: {id}\n{str(e)}')
         return stocks
