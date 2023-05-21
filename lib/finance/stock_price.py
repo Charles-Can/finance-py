082620eba6aba94679ec17f4bddfb7b2c6aa02e5
Charles Candelaria <charles-can@outlook.com>
2023-05-20 21:17:22 -0600

    documents

--- lib/finance/stock_price.py
+++ lib/finance/stock_price.py
@@ -1,3 +1,8 @@
+"""
+    Author: Charles Candelaria
+    Date: 05/20/2023
+    Functionality: StockPrice Class
+"""
 from datetime import datetime
 
 
@@ -5,15 +10,22 @@ class StockPrice:
     def __init__(self, symbol='', date='', open='-', high='-', low='-', close='-', volume=0):
         self.id = None
         self.symbol = symbol
+        """Stock symbol"""
         self.date = date
+        """date string d-b-y"""
         self.open = open
+        """Opening price"""
         self.high = high
+        """Peak price"""
         self.low = low
+        """Lowest price"""
         self.close = close
+        """closing price"""
         self.volume = volume
 
     @property
     def recorded_date(self):
+        """date as datetime""" 
         try:
             return datetime.strptime(self.date, '%d-%b-%y')
         except Exception as e:
