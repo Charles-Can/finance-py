df3e0b9bf814d5d90e0dcf95ff83e8e6b42bf287
Charles Candelaria <charles-can@outlook.com>
2023-05-20 21:40:32 -0600

    adds comments

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ BONDS_FILE_PATH = './files/Lesson6_Data_Bonds.csv'
 STOCKS_FILE_PATH = './files/Lesson6_Data_Stocks.csv'
 STOCK_PRICES_FILE_PATH = './files/AllStocks.json'
 
+# app contsants
 CHART_STYLE = 'Solarize_Light2'
 SAVE_FOLDER = './out_files/'
 
+# Setup database objects
 db = sqlite3.connect(':memory:')
 
 investor_store = InvestorsStore(db)
@@ -28,7 +30,7 @@ prices_store = StockPricesStore(db)
 
 
 def insert_stocks_from_file(file_path: str, investor_id: int):
-    # get investment data
+    """inserts stock data into store and associates it with an investor"""
     stocks = import_stocks(file_path)
     for stock in stocks:
         stock.investor_id = investor_id
@@ -36,6 +38,7 @@ def insert_stocks_from_file(file_path: str, investor_id: int):
 
 
 def insert_bonds_from_file(file_path: str, investor_id: int):
+    """inserts bond data into store and associates it with an investor"""
     bonds = import_bonds(file_path)
     for bond in bonds:
         bond.investor_id = investor_id
@@ -43,6 +46,7 @@ def insert_bonds_from_file(file_path: str, investor_id: int):
 
 
 def insert_prices_from_file(file_path: str):
+    """inserts stock price data into store"""
     prices = import_stock_price_history(file_path)
 
     for price in prices:
@@ -50,24 +54,35 @@ def insert_prices_from_file(file_path: str):
 
 
 def build_stock_plot_chart_by_investor(investor: Investor) -> tuple[fig.Figure, axes.Axes]:
-    fig, ax = plt.subplots()
+    """Builds stock plot chart"""
+    f, ax = plt.subplots()
+    # set size
+    f.set_size_inches(8, 6)
 
+    # get stocks for investor
     stocks = stock_store.select_by_investor_id(investor.id)
 
     for stock in stocks:
+        # Get prices for the stock symbol
         prices = prices_store.select_by_symbol(stock.symbol)
+        # pull dates
         dates = [pr.recorded_date for pr in prices]
+        # pull values * shares
         stock_values = [float(pr.close) * stock.shares for pr in prices]
 
         plt.plot(dates, stock_values, label=stock.symbol)
 
     # Format Chart display
-    plt.title(f'stock values over time for {investor.name}'.title(), fontsize=18)
+    # titles
+    plt.title(
+        f'stock values over time for {investor.name}'.title(), fontsize=18)
     ax.set_xlabel('Date Recorded', fontsize=14)
     ax.set_ylabel('Shares Value', fontsize=14)
+    # set max xticks for chart
     ax.xaxis.set_major_locator(plt.MaxNLocator(8))
-    plt.legend(loc='upper left')
     plt.xticks(rotation=45)
+    # legend
+    plt.legend(loc='upper left')
 
     return (fig, ax)
 
@@ -77,20 +92,27 @@ def main():
     # Create investor
     investor = Investor(name='Bob Smith', address='123 fake St.',
                         phone_number='123-123-1234')
+    # save investor to store
     investor_store.insert(investor)
 
+    # import data
     insert_stocks_from_file(STOCKS_FILE_PATH, investor.id)
     insert_bonds_from_file(BONDS_FILE_PATH, investor.id)
     insert_prices_from_file(STOCK_PRICES_FILE_PATH)
 
+    # set chart style
     plt.style.use(CHART_STYLE)
 
     build_stock_plot_chart_by_investor(investor)
 
+    # create filename for investor
     today = datetime.today().strftime('%d_%m_%y').lower()
-    plt.savefig(f'{SAVE_FOLDER}{investor.name.replace(" ", "_").lower()}_{today}.pdf')
+
+    plt.savefig(
+        f'{SAVE_FOLDER}{investor.name.replace(" ", "_").lower()}_{today}.pdf')
     plt.show()
 
+    # close db connection
     db.close()
 
 
